1d0dc6ceb62d79bf86ec1ee1f3f6a4f7afb826a0
NanashiCheater <99939341+NanashiCheater@users.noreply.github.com>
2022-03-24 12:27:23 +0500

    Update hidebox.js

--- components/services/hidebox.js
+++ components/services/hidebox.js
@@ -7,3 +7,18 @@ $(this).hide("slow");
 $(".main__two-column__element h3").click(function () {
 $(this).next("p").show("slow");
 });
+
+$(".main__two-column__element img").hover(
+				function() {
+					$(this).animate({
+						width: "36px",
+						height: "36px",
+						borderRadius: "2%"
+					}, "slow");
+				}, function() {
+					$(this).animate({
+						width: "32px",
+						height: "32px",
+						borderRadius: "10%"
+					}, "slow");
+				});
